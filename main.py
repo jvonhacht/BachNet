035b2b87dbd9d92e7e6fad28a23dc7cc6c1a9caa
David Stevens <dstevens@kth.se>
2020-05-06 21:52:20 +0200

    Add GPU handler

--- main.py
+++ main.py
@@ -9,6 +9,16 @@ from itertools import cycle
 # Fix seed for reproducibility
 tf.random.set_seed(0)
 
+gpus = tf.config.experimental.list_physical_devices('GPU')
+if gpus:
+    try:
+        for gpu in gpus:
+            tf.config.experimental.set_memory_growth(gpu, True)
+            logical_gpus = tf.config.experimental.list_logical_devices('GPU')
+            print(len(gpus), "Physical GPUs,", len(logical_gpus), "Logical GPUs")
+    except RuntimeError as e:
+        print(e)
+
 
 class OneHotEncoder:
     def __init__(self, train, test, val):
