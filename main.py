73e28aa506b44d6dd6ce6dd441d1e12da52d13d7
David Stevens <dstevens@kth.se>
2020-05-07 00:18:37 +0200

    Experiment with seq2seq

--- main.py
+++ main.py
@@ -1,6 +1,15 @@
 import numpy as np
 from typing import Tuple, Iterable
 import tensorflow as tf
+from tensorflow.keras.layers import (
+    LSTM,
+    Reshape,
+    Dense,
+    Activation,
+    RepeatVector,
+    Input,
+)
+from tensorflow.keras import Model
 import matplotlib.pyplot as plt
 from tqdm import tqdm
 from sklearn.preprocessing import StandardScaler
@@ -9,12 +18,12 @@ from itertools import cycle
 # Fix seed for reproducibility
 tf.random.set_seed(0)
 
-gpus = tf.config.experimental.list_physical_devices('GPU')
+gpus = tf.config.experimental.list_physical_devices("GPU")
 if gpus:
     try:
         for gpu in gpus:
             tf.config.experimental.set_memory_growth(gpu, True)
-            logical_gpus = tf.config.experimental.list_logical_devices('GPU')
+            logical_gpus = tf.config.experimental.list_logical_devices("GPU")
             print(len(gpus), "Physical GPUs,", len(logical_gpus), "Logical GPUs")
     except RuntimeError as e:
         print(e)
@@ -25,7 +34,7 @@ class OneHotEncoder:
         self.lowest = int(
             min(
                 [
-                    min([np.min(piece) for piece in dataset])
+                    min([np.nanmin(piece) for piece in dataset])
                     for dataset in (train, test, val)
                 ]
             )
@@ -33,7 +42,7 @@ class OneHotEncoder:
         self.highest = int(
             max(
                 [
-                    max([np.max(piece) for piece in dataset])
+                    max([np.nanmax(piece) for piece in dataset])
                     for dataset in (train, test, val)
                 ]
             )
@@ -68,7 +77,15 @@ class OneHotEncoder:
         )
 
     def softmax_to_midi(self, softmax: np.ndarray) -> float:
-        one_hot = np.asarray([[self.midi_note_one_hot(np.random.choice(self.n_notes, p=voice)) for voice in beat]for beat in softmax])
+        one_hot = np.asarray(
+            [
+                [
+                    self.midi_note_one_hot(np.random.choice(self.n_notes, p=voice))
+                    for voice in beat
+                ]
+                for beat in softmax
+            ]
+        )
         return self.decode_song(one_hot)
 
 
@@ -76,44 +93,80 @@ def main():
     data = np.load("data/Jsb16thSeparated.npz", allow_pickle=True, encoding="latin1")
     train, test, val = data["train"], data["test"], data["valid"]
 
+    max_len = max(
+        map(lambda dataset: max(map(lambda x: x.shape[0], dataset)), (train, test, val))
+    )
+    for dataset in (train, test, val):
+        for i, piece in enumerate(dataset):
+            padded = np.nan * np.ones((max_len, 4))
+            padded[: piece.shape[0], :] = piece
+            dataset[i] = padded
+
     encoder = OneHotEncoder(train, test, val)
-    one_hot_train, one_hot_test, one_hot_val = [[encoder.encode_song(x) for x in dataset] for dataset in (train, test, val)]
-    
-    model = tf.keras.Sequential()
+    one_hot_train, one_hot_test, one_hot_val = [
+        [encoder.encode_song(x) for x in dataset] for dataset in (train, test, val)
+    ]
+
     x_train, x_test, x_val = [
         [x[:, 0:1, :] for x in dataset]
         for dataset in (one_hot_train, one_hot_test, one_hot_val)
     ]
-    y_train, y_test, y_val = [[y[:, 1:, :] for y in dataset]
+    y_train, y_test, y_val = [
+        [y[:, 1:, :] for y in dataset]
         for dataset in (one_hot_train, one_hot_test, one_hot_val)
     ]
-    input_dim = x_train[0].shape[1:]
     output_dim = y_train[0].shape[1:]
-    batch_size = 8
+    n_notes = x_train[0].shape[-1]
+    batch_size = max_len
 
-    model.add(tf.keras.layers.LSTM(46, input_shape=(x_train[0].shape[1:]), return_sequences=True))
-    model.add(tf.keras.layers.Reshape((reversed(input_dim))))
-    model.add(tf.keras.layers.Dense(3))
-    model.add(tf.keras.layers.Reshape(output_dim))
-    model.add(tf.keras.layers.Activation('softmax'))
+    latent_unit_count = 256
+    # Define an input sequence and process it.
+    encoder_inputs = Input(shape=(None, n_notes))
+    # Units set arbitrarily
+    encoder = LSTM(units=latent_unit_count, return_state=True)
+    encoder_outputs, state_h, state_c = encoder(encoder_inputs)
+    # We discard `encoder_outputs` and only keep the states.
+    encoder_states = [state_h, state_c]
+
+    # Set up the decoder, using `encoder_states` as initial state.
+    decoder_inputs = Input(shape=(None, n_notes))
+    # We set up our decoder to return full output sequences,
+    # and to return internal states as well. We don't use the
+    # return states in the training model, but we will use them in inference.
+    decoder_lstm = LSTM(latent_unit_count, return_sequences=True, return_state=True)
+    decoder_outputs, _, _ = decoder_lstm(decoder_inputs, initial_state=encoder_states)
+    decoder_dense = Dense(n_notes, activation="softmax")
+    decoder_outputs = decoder_dense(decoder_outputs)
+
+    # Define the model that will turn
+    # `encoder_input_data` & `decoder_input_data` into `decoder_target_data`
+    model = Model([encoder_inputs, decoder_inputs], decoder_outputs)
+
     model.summary()
-    model.compile(loss='categorical_crossentropy', optimizer='adam')
+    model.compile(loss="categorical_crossentropy", optimizer="adam")
     # We train separately on each song, but the weights are maintained.
-    history = {'loss': [], 'val_loss': []}
+    history = {"loss": [], "val_loss": []}
     epochs = 10
     for epoch in range(epochs):
-        for melody, harmony, val_melody, val_harmony in zip(x_train, y_train, cycle(x_val), cycle(y_val)):
-            hist = model.fit(melody, harmony, epochs=1, batch_size=batch_size, validation_data=(val_melody, val_harmony))
-            history['loss'] += hist.history['loss']
-            history['val_loss'] += hist.history['val_loss']
+        for melody, harmony, val_melody, val_harmony in zip(
+            x_train, y_train, cycle(x_val), cycle(y_val)
+        ):
+            hist = model.fit(
+                melody,
+                harmony,
+                epochs=1,
+                validation_data=(val_melody, val_harmony),
+                batch_size=max_len,
+            )
+            history["loss"] += hist.history["loss"]
+            history["val_loss"] += hist.history["val_loss"]
             model.reset_states()
     y_hat = model.predict(x_train[0])
     song = encoder.softmax_to_midi(y_hat)
-    plt.plot(history['loss'], label='Training loss')
-    plt.plot(history['val_loss'], label='Validation loss')
+    plt.plot(history["loss"], label="Training loss")
+    plt.plot(history["val_loss"], label="Validation loss")
     plt.legend()
     plt.show()
-
 
 
 if __name__ == "__main__":
