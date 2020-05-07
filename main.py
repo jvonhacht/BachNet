3a2cadb0ee08a09c5026a4e23e73aa8690ca2ccc
David Stevens <dstevens@kth.se>
2020-05-07 12:06:25 +0200

    seq2seq?

--- main.py
+++ main.py
@@ -9,7 +9,7 @@ from tensorflow.keras.layers import (
     RepeatVector,
     Input,
 )
-from tensorflow.keras import Model
+from tensorflow.keras import Model, Sequential
 import matplotlib.pyplot as plt
 from tqdm import tqdm
 from sklearn.preprocessing import StandardScaler
@@ -120,30 +120,16 @@ def main():
     batch_size = max_len
 
     latent_unit_count = 256
-    # Define an input sequence and process it.
-    encoder_inputs = Input(shape=(1, n_notes))
-    # Units set arbitrarily
-    encoder = LSTM(units=latent_unit_count, return_state=True)
-    encoder_outputs, state_h, state_c = encoder(encoder_inputs)
-    # We discard `encoder_outputs` and only keep the states.
-    encoder_states = [state_h, state_c]
-
-    # Set up the decoder, using `encoder_states` as initial state.
-    decoder_inputs = Input(shape=(None, n_notes))
-    # We set up our decoder to return full output sequences,
-    # and to return internal states as well. We don't use the
-    # return states in the training model, but we will use them in inference.
-    decoder_lstm = LSTM(latent_unit_count, return_sequences=True, return_state=True)
-    decoder_outputs, _, _ = decoder_lstm(decoder_inputs, initial_state=encoder_states)
-    decoder_dense = Dense(n_notes, activation="softmax")
-    decoder_outputs = decoder_dense(decoder_outputs)
-
-    # Define the model that will turn
-    # `encoder_input_data` & `decoder_input_data` into `decoder_target_data`
-    model = Model([encoder_inputs, decoder_inputs], decoder_outputs)
-
-    model.summary()
+    model = Sequential()
+    model.add(LSTM(units=latent_unit_count, input_shape=(1, n_notes), name='encoder'))
+    model.add(Reshape((1, latent_unit_count)))
+    model.add(LSTM(units=latent_unit_count, return_sequences=True, name='decoder'))
+    model.add(Dense(n_notes * 3))
+    model.add(Reshape((3, n_notes)))
+    model.add(Activation('softmax'))
     model.compile(loss="categorical_crossentropy", optimizer="adam")
+    model.summary()
+
     # We train separately on each song, but the weights are maintained.
     history = {"loss": [], "val_loss": []}
     epochs = 10
