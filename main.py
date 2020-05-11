d1a572c8714b136bbb645f94e7ec0bee62cac1d8
David Stevens <dstevens@kth.se>
2020-05-11 03:44:49 +0200

    Implement attention based model

--- main.py
+++ main.py
@@ -12,12 +12,16 @@ from tensorflow.keras.layers import (
 from tensorflow.keras import Model, Sequential
 import matplotlib.pyplot as plt
 from sklearn.utils import shuffle
-from models import BachNet
+from models import BachNet, Encoder, Decoder, BahdanauAttention
 from dataclasses import dataclass
 import os
+import json
 
 from midi import MidiConverter
 import sys
+import time
+from tqdm import tqdm
+from collections import defaultdict
 # Fix seed for reproducibility
 tf.random.set_seed(0)
 
@@ -194,48 +198,123 @@ def load_data(shuffle_data=False, augment=False, mode='pad') -> Dataset:
 
 
 def main():
-    x, y, encoder = load_data(shuffle_data=True, augment=True, mode='crop')
-    output_dim = y[0].shape[1:]
-    n_notes = x[0].shape[-1]
+    latent_unit_count = 256
+    EPOCHS = 25
+    BATCH_SIZE = 8
 
-    latent_unit_count = 1024
-    model = BachNet(latent_unit_count)
-    _ = model(x[0:1])
-    model.compile(loss="categorical_crossentropy", optimizer="adam")
-    model.summary()
-    tf.keras.utils.plot_model(model, to_file='model.png', dpi=300, show_shapes=True, show_layer_names=True, rankdir='LR', expand_nested=True)
-
-    checkpoint_dir = './training_checkpoints'
-    checkpoint_prefix = os.path.join(checkpoint_dir, "ckpt")
-    callbacks = [
-        # tf.keras.callbacks.EarlyStopping(patience=5),
-        tf.keras.callbacks.ModelCheckpoint(filepath=checkpoint_dir+'/model.{epoch:02d}-{val_loss:.2f}.h5', monitor='val_loss'),
-        # tf.keras.callbacks.TensorBoard(log_dir='./logs'),
-    ]
-    EPOCHS = 100
-    BATCH_SIZE = 64
+    x, y, dataset_encoder = load_data(shuffle_data=True, augment=True, mode='crop')
+    n_songs = x.shape[0]
+    song_len, n_notes = x[0].shape
     validation_split = .2
-    validation_idx = int(x.shape[0] * (1 - validation_split))
+    validation_idx = int(n_songs * (1 - validation_split))
     training_data = tf.data.Dataset.from_tensor_slices((x[:validation_idx], y[:validation_idx]))
     training_data = training_data.batch(BATCH_SIZE)
     validation_data = tf.data.Dataset.from_tensor_slices((x[validation_idx:], y[validation_idx:]))
     validation_data = validation_data.batch(BATCH_SIZE)
 
-    # model.load_weights('model.11-1.40.h5')
-    hist = model.fit(training_data, callbacks=callbacks, epochs=EPOCHS, validation_data=validation_data)
-    model.save_weights("model.h5")
+    encoder = Encoder(latent_unit_count, BATCH_SIZE)
+    attention_layer = BahdanauAttention(10)
+    decoder = Decoder(latent_unit_count,BATCH_SIZE,n_notes)
 
-    plt.plot(hist.history["loss"], label="Training loss")
-    plt.plot(hist.history["val_loss"], label="Validation loss")
-    plt.legend()
-    plt.savefig('model_loss.png', dpi=300)
+    optimizer = tf.keras.optimizers.Adam()
+    loss_object = tf.keras.losses.CategoricalCrossentropy(
+        from_logits=True, reduction='none')
 
-    y_hat = model.predict(x[0:1])[0]
-    song = encoder.softmax_to_midi(y_hat, mode='argmax')
+    def loss_function(real, pred):
+        loss_ = loss_object(real, pred)
+        return tf.reduce_mean(loss_, axis=None)
+
+    # @tf.function # BUGGY ON MY MACHINE
+    def train_step(inp, targ, enc_hidden, mode='train'):
+        loss = 0
+        with tf.GradientTape() as tape:
+            enc_output, enc_hidden = encoder(inp, enc_hidden)
+            dec_hidden = enc_hidden
+            # Begin with silence?
+            dec_input = tf.expand_dims([[dataset_encoder.midi_note_one_hot(np.nan)]*3] * BATCH_SIZE, 1)
+            # Teacher forcing - feeding the target as the next input
+            for t in range(1, targ.shape[1]):
+                # passing enc_output to the decoder
+                predictions, dec_hidden, _ = decoder(dec_input, dec_hidden, enc_output)
+                loss += loss_function(targ[:, t], predictions)
+                # using teacher forcing
+                dec_input = tf.expand_dims(targ[:, t], 1)
+        batch_loss = (loss / int(targ.shape[1]))
+        if mode == 'train':
+            variables = encoder.trainable_variables + decoder.trainable_variables
+            gradients = tape.gradient(loss, variables)
+            optimizer.apply_gradients(zip(gradients, variables))
+
+        return batch_loss
+
+    def harmonize(melody):
+        attention_plot = np.zeros((BATCH_SIZE, song_len, song_len))
+        result = np.zeros((BATCH_SIZE, song_len, 3))
+        softmax = np.zeros((BATCH_SIZE, song_len,3,n_notes))
+        hidden = encoder.initialize_hidden_state()
+        enc_out, enc_hidden = encoder(melody, hidden)
+        dec_hidden = enc_hidden
+        dec_input = tf.expand_dims([[dataset_encoder.midi_note_one_hot(np.nan)]*3] * BATCH_SIZE, 1)
+        for t in range(song_len):
+            predictions, dec_hidden, attention_weights = decoder(dec_input, dec_hidden, enc_out)
+            # storing the attention weights to plot later on
+            # attention_weights = tf.reshape(attention_weights, (-1, ))
+            # attention_plot[t] = attention_weights.numpy()
+
+            predicted_idxs = tf.argmax(predictions, axis=2).numpy()
+            predicted_notes = predicted_idxs + dataset_encoder.lowest
+            next_input = np.zeros_like(dec_input)
+            for i, song in enumerate(predicted_notes):
+                for j, note in enumerate(song):
+                    one_hot_idx = note - dataset_encoder.lowest
+                    next_input[i,0,j,one_hot_idx] = 1
+                    if note == n_notes - 1:
+                        predicted_notes[i,j] = np.nan
+            result[:,t] = predicted_idxs + dataset_encoder.lowest
+            softmax[:,t] = predictions
+            # Feed predicted notes back into model
+            dec_input = next_input
+        return result, melody, attention_plot, softmax
+
+    # Train
+    steps_per_epoch = validation_idx // BATCH_SIZE
+    val_steps_per_epoch = (n_songs - validation_idx) // BATCH_SIZE
+
+    loss_history = defaultdict(list)
+    for epoch in range(EPOCHS):
+        start = time.time()
+        enc_hidden = encoder.initialize_hidden_state()
+        total_loss = 0
+        for batch, (inp, targ) in tqdm(enumerate(training_data.take(steps_per_epoch)), total=steps_per_epoch, desc=f"Training epoch {epoch+1}"):
+            batch_loss = train_step(inp, targ, enc_hidden)
+            total_loss += batch_loss
+            tqdm.write(f"Current loss: {total_loss/batch:.4f}")
+            loss_history[epoch].append(float(batch_loss.numpy()))
+        total_val_loss = 0
+        with open('history.json', 'w') as out:
+            json.dump(loss_history, out)
+        # # Compute validation loss
+        # enc_hidden = encoder.initialize_hidden_state()
+        # for batch, (inp, targ) in tqdm(enumerate(validation_data.take(val_steps_per_epoch)), total=val_steps_per_epoch, desc=f"Calculating validation loss"):
+        #     *_, softmax = harmonize()
+        #     batch_loss = train_step(inp, targ, enc_hidden, mode='test')
+        #     total_val_loss += batch_loss
+        print(f"Epoch {epoch+1} Loss {total_loss/steps_per_epoch:.4f} Validation loss {total_val_loss/val_steps_per_epoch:.4f} Took {(time.time()-start):.1f}s")
+        encoder.save_weights('encoder.h5')
+        decoder.save_weights('decoder.h5')
+
+    # Save weights
+    enc_hidden = encoder.initialize_hidden_state()
+    inp, targ = next(iter(training_data))
+    train_step(inp, targ, enc_hidden)
+    encoder.save_weights('encoder.h5')
+    decoder.save_weights('decoder.h5')
+    y_hats, *_, softmaxes = harmonize(x[0:BATCH_SIZE])
+    song = dataset_encoder.softmax_to_midi(softmaxes[0], mode='argmax')
     
     midi_converter = MidiConverter()
-    original_melody = encoder.softmax_to_midi(x[0][:, None, :])
-    original_harmony = encoder.softmax_to_midi(y[0])
+    original_melody = dataset_encoder.softmax_to_midi(x[0][:, None, :])
+    original_harmony = dataset_encoder.softmax_to_midi(y[0])
     melody_and_song = np.concatenate((original_melody, song), axis=1)
     midi_converter.convert_to_midi(melody_and_song, 'model_out', resolution=1/4, tempo=60)
     original_song = np.concatenate((original_melody, original_harmony), axis=1)
--- models.py
+++ models.py
@@ -2,6 +2,118 @@ import tensorflow as tf
 from tensorflow.keras.layers import Dense, Flatten, GRU, LSTM, Layer, Activation, RepeatVector
 from tensorflow.keras import Model
 
+class Encoder(tf.keras.Model):
+  def __init__(self, enc_units, batch_sz):
+    super(Encoder, self).__init__()
+    self.batch_sz = batch_sz
+    self.enc_units = enc_units
+    self.a_gru = tf.keras.layers.GRU(self.enc_units,
+                                   return_sequences=True,
+                                   return_state=True,
+                                   recurrent_initializer='glorot_uniform')
+    self.t_gru = tf.keras.layers.GRU(self.enc_units,
+                                   return_sequences=True,
+                                   return_state=True,
+                                   recurrent_initializer='glorot_uniform')
+    self.b_gru = tf.keras.layers.GRU(self.enc_units,
+                                   return_sequences=True,
+                                   return_state=True,
+                                   recurrent_initializer='glorot_uniform')
+
+  def call(self, x, hidden):
+    a_output, a_state = self.a_gru(x, initial_state = hidden[0])
+    t_output, t_state = self.t_gru(x, initial_state = hidden[1])
+    b_output, b_state = self.b_gru(x, initial_state=hidden[2])
+    output = tf.stack((a_output,t_output,b_output))
+    state = tf.stack((a_state,t_state,b_state))
+    return output, state
+
+  def initialize_hidden_state(self):
+    return tf.zeros((3,self.batch_sz, self.enc_units))
+
+
+class BahdanauAttention(tf.keras.layers.Layer):
+  def __init__(self, units):
+    super(BahdanauAttention, self).__init__()
+    self.aW1 = tf.keras.layers.Dense(units)
+    self.aW2 = tf.keras.layers.Dense(units)
+    self.aV = tf.keras.layers.Dense(1)
+    self.tW1 = tf.keras.layers.Dense(units)
+    self.tW2 = tf.keras.layers.Dense(units)
+    self.tV = tf.keras.layers.Dense(1)
+    self.bW1 = tf.keras.layers.Dense(units)
+    self.bW2 = tf.keras.layers.Dense(units)
+    self.bV = tf.keras.layers.Dense(1)
+
+  def call(self, query, values):
+    # query hidden state shape == (batch_size, hidden size)
+    # query_with_time_axis shape == (batch_size, 1, hidden size)
+    # values shape == (batch_size, max_len, hidden size)
+    # we are doing this to broadcast addition along the time axis to calculate the score
+    query_with_time_axis = tf.expand_dims(query, 2)
+
+    # score shape == (batch_size, max_length, 1)
+    # we get 1 at the last axis because we are applying score to self.V
+    # the shape of the tensor before applying self.V is (batch_size, max_length, units)
+    a_score = self.aV(tf.nn.tanh(self.aW1(query_with_time_axis[0])+self.aW2(values[0])))
+    t_score = self.tV(tf.nn.tanh(self.tW1(query_with_time_axis[1])+self.tW2(values[1])))
+    b_score = self.bV(tf.nn.tanh(self.bW1(query_with_time_axis[2])+self.bW2(values[2])))
+    
+    a_weights = tf.nn.softmax(a_score, axis=1)
+    t_weights = tf.nn.softmax(t_score, axis=1)
+    b_weights = tf.nn.softmax(b_score, axis=1)
+
+    a_context = tf.reduce_sum(a_weights * values[0], axis=1)
+    t_context = tf.reduce_sum(t_weights * values[1], axis=1)
+    b_context = tf.reduce_sum(b_weights * values[2], axis=1)
+
+    return tf.stack((a_context,t_context,b_context)), tf.stack((a_weights,t_weights,b_weights))
+
+
+class Decoder(tf.keras.Model):
+  def __init__(self, dec_units, batch_sz, n_notes):
+    super(Decoder, self).__init__()
+    self.batch_sz = batch_sz
+    self.dec_units = dec_units
+    self.alto = tf.keras.layers.GRU(self.dec_units,
+                                   return_sequences=True,
+                                   return_state=True,
+                                   recurrent_initializer='glorot_uniform')
+    self.tenor = tf.keras.layers.GRU(self.dec_units,
+                                   return_sequences=True,
+                                   return_state=True,
+                                   recurrent_initializer='glorot_uniform')
+    self.bass = tf.keras.layers.GRU(self.dec_units,
+                                   return_sequences=True,
+                                   return_state=True,
+                                   recurrent_initializer='glorot_uniform')
+    self.fc = tf.keras.layers.Dense(n_notes)
+    self.activation = Activation('softmax')
+    # used for attention
+    self.attention = BahdanauAttention(self.dec_units)
+
+  def call(self, x, hidden, enc_output):
+    # enc_output shape == (batch_size, max_length, hidden_size)
+    context_vector, attention_weights = self.attention(hidden, enc_output)
+
+    # passing the concatenated vector to the GRUs
+
+    a, a_state = self.alto(tf.concat([tf.expand_dims(context_vector[0], 1), x[:,:,0,:]], axis=-1))
+    t, t_state = self.tenor(tf.concat([tf.expand_dims(context_vector[1], 1), x[:,:,1,:]], axis=-1))
+    b, b_state = self.bass(tf.concat([tf.expand_dims(context_vector[2], 1), x[:,:,2,:]], axis=-1))
+    a = tf.squeeze(a, axis=1)
+    t = tf.squeeze(t, axis=1)
+    b = tf.squeeze(b, axis=1)
+
+    # output shape == (batch_size * 1, hidden_size)
+    # output = tf.reshape(output, (-1, output.shape[2]))
+    x = tf.stack([a,t,b], axis=1)
+    # output shape == (batch_size, vocab)
+    x = self.fc(x)
+    x = self.activation(x)
+    return x, tf.stack((a_state, t_state, b_state)), attention_weights
+
+
 class BachNet(Model):
     def __init__(self, latent_units, *args, **kwargs):
         super().__init__(*args, **kwargs)
