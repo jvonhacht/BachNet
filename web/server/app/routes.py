7ceae762c3167d5bfb79e95c9577501ca0319560
David Johansson <davidjo2@kth.se>
2020-05-16 08:02:34 +0200

    add placeholder template

--- web/server/app/routes.py
+++ web/server/app/routes.py
@@ -1,9 +1,10 @@
 from app import app
 from bach_net import harmonize_json
+from flask import render_template, url_for
 
 @app.route('/')
 def hello():
-    return "Hello, world!"
+    return render_template('index.html')
 
 @app.route('/harmonize', methods=['POST'])
 def handle_harmonize(melody):
