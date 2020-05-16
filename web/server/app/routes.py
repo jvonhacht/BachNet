37585db8c4eb931b72d3ad2b53cce2214f80c560
David Stevens <dstevens@kth.se>
2020-05-16 18:09:53 +0200

    Change docker file

--- web/server/app/routes.py
+++ web/server/app/routes.py
@@ -1,10 +1,20 @@
 from app import app
-from bach_net import harmonize_json
+import app.performer as performer
+from flask import request, jsonify
+from numpy import nan
+
+harmonizer = performer.Harmonizer()
 
 @app.route('/')
 def hello():
     return "Hello, world!"
 
 @app.route('/harmonize', methods=['POST'])
-def handle_harmonize(melody):
-    return harmonize_json()
+def handle_harmonize():
+    req = request.json
+    melody = req['melody']
+    melody = [[beat if beat is not None else nan] for beat in melody]
+    required_length = 100
+    melody = melody[:required_length] + [nan] * (required_length - len(melody)) # Crop and pad
+    harmonization = harmonizer.harmonize(melody, req['overfit'])
+    return jsonify({"harmonies":harmonization})
